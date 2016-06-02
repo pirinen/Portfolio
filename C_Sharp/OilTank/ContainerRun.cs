8b51c844d8db6a5203383fd68bafe319d6004d48
pirinen <1500912@edu.karelia.fi>
2016-06-02 13:17:28 +0300

    Create ContainerRun.cs

---
+++ C_Sharp/OilTank/ContainerRun.cs
@@ -0,0 +1,19 @@
+using System; 
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Container1._0
+{ //Jukka Pirinen 2.6.2016
+    class Container1
+    {
+
+        static void Main(string[] args)
+        {
+            new Application().Run();
+            
+        }
+    }
+}
+
