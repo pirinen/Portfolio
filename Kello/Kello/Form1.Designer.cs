489ff039498cb57cb0a41b6428ae3a860f293786
pirinen <1500912@edu.karelia.fi>
2018-01-21 19:10:50 +0200

    Add files via upload

---
+++ Kello/Kello/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace Kello
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.StartBtn = new System.Windows.Forms.Button();
+            this.StopBtn = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.infoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SaveBtn = new System.Windows.Forms.Button();
+            this.ReStartBtn = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // StartBtn
+            // 
+            this.StartBtn.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.StartBtn.Location = new System.Drawing.Point(362, 432);
+            this.StartBtn.Name = "StartBtn";
+            this.StartBtn.Size = new System.Drawing.Size(75, 23);
+            this.StartBtn.TabIndex = 0;
+            this.StartBtn.Text = "Start";
+            this.StartBtn.UseVisualStyleBackColor = false;
+            this.StartBtn.Click += new System.EventHandler(this.StartBtn_Click);
+            // 
+            // StopBtn
+            // 
+            this.StopBtn.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.StopBtn.Location = new System.Drawing.Point(449, 432);
+            this.StopBtn.Name = "StopBtn";
+            this.StopBtn.Size = new System.Drawing.Size(75, 23);
+            this.StopBtn.TabIndex = 1;
+            this.StopBtn.Text = "Stop";
+            this.StopBtn.UseVisualStyleBackColor = false;
+            this.StopBtn.Click += new System.EventHandler(this.StopBtn_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.infoToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(536, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.closeToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.closeToolStripMenuItem.Text = "Close";
+            // 
+            // infoToolStripMenuItem
+            // 
+            this.infoToolStripMenuItem.Name = "infoToolStripMenuItem";
+            this.infoToolStripMenuItem.Size = new System.Drawing.Size(40, 20);
+            this.infoToolStripMenuItem.Text = "Info";
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(60, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(405, 93);
+            this.label1.TabIndex = 3;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(60, 177);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(405, 93);
+            this.label2.TabIndex = 4;
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(60, 298);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(405, 93);
+            this.label3.TabIndex = 5;
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // SaveBtn
+            // 
+            this.SaveBtn.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.SaveBtn.Location = new System.Drawing.Point(12, 432);
+            this.SaveBtn.Name = "SaveBtn";
+            this.SaveBtn.Size = new System.Drawing.Size(75, 23);
+            this.SaveBtn.TabIndex = 6;
+            this.SaveBtn.Text = "Save";
+            this.SaveBtn.UseVisualStyleBackColor = false;
+            this.SaveBtn.Click += new System.EventHandler(this.SaveBtn_Click);
+            // 
+            // ReStartBtn
+            // 
+            this.ReStartBtn.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ReStartBtn.Location = new System.Drawing.Point(193, 432);
+            this.ReStartBtn.Name = "ReStartBtn";
+            this.ReStartBtn.Size = new System.Drawing.Size(75, 23);
+            this.ReStartBtn.TabIndex = 7;
+            this.ReStartBtn.Text = "ReStart";
+            this.ReStartBtn.UseVisualStyleBackColor = false;
+            this.ReStartBtn.Click += new System.EventHandler(this.ReStartBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(536, 467);
+            this.Controls.Add(this.ReStartBtn);
+            this.Controls.Add(this.SaveBtn);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.StopBtn);
+            this.Controls.Add(this.StartBtn);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "KelloApp";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button StartBtn;
+        private System.Windows.Forms.Button StopBtn;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem infoToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button SaveBtn;
+        private System.Windows.Forms.Button ReStartBtn;
+    }
+}
+
---
+++ Kello/Kello/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Threading;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Kello
+{
+    public partial class Form1 : Form
+    {
+        Stopwatch stopWatch = new Stopwatch();
+        Stopwatch stopWatchTotal = new Stopwatch();
+        bool ajastin = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Text = DateTime.Now.ToString("HH:mm:ss");     //Nykyinen aika
+            if (ajastin == true)
+            {               
+                label2.Text = stopWatch.Elapsed.ToString(); //Juokseva aika
+            }
+        }
+
+        private void StartBtn_Click(object sender, EventArgs e)
+        {
+            ajastin = true;
+            stopWatch.Start();
+            stopWatchTotal.Start();
+        }
+
+        private void StopBtn_Click(object sender, EventArgs e)
+        {
+            ajastin = false;
+            stopWatch.Stop();
+            stopWatchTotal.Stop();
+            label2.Text = stopWatch.Elapsed.ToString();
+            label3.Text = stopWatchTotal.Elapsed.ToString();
+            stopWatch.Reset();  //Nollataan, että saadaan juokseva aika keskelle alkamaan alusta
+
+        }
+
+        private void SaveBtn_Click(object sender, EventArgs e)
+        {
+            File.AppendAllText("C:\\Users\\pirin\\Omat\\Ohjelmat\\ajat.txt", "Totaltime: " + (stopWatchTotal.Elapsed.ToString() + " - Aika: " + DateTime.Now.ToString("dd.MM.yyyy HH:mm:ss")) + Environment.NewLine);
+            MessageBox.Show("Tiedot tallennettu", "Tallennus");
+            /*
+            using (StreamWriter writer = new StreamWriter("C:\\Users\\pirin\\Omat\\Ohjelmat\\ajat.txt"))
+            {
+                writer.WriteLine(stopWatchTotal.Elapsed.ToString() + " " + DateTime.Now.ToString("dd.MM.yyyy HH:mm:ss"));
+                MessageBox.Show("Tiedot tallennettu","Tallennus");
+            }*/
+        }
+
+        private void ReStartBtn_Click(object sender, EventArgs e)
+        {
+            stopWatch.Reset();
+            stopWatchTotal.Reset();
+            label2.Text = stopWatch.Elapsed.ToString();
+            label3.Text = stopWatch.Elapsed.ToString();
+        }
+    }
+}
